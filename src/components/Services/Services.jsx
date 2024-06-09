27e0a5dff2b903481ecee635b2e6b25ed16afe77
timubin <mubin3242@gmail.com>
2024-06-09 23:37:56 +0600

    add 2 protfolio

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,96 @@
+import React, { useContext } from "react";
+import "./Services.css";
+import Card from "../Card/Card";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import Resume from './resume.pdf';
+import Button from "../Button/Button";
+
+const Services = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
+
+  return (
+    <div className="services" id="services">
+      {/* left side */}
+      <div className="awesome">
+        {/* dark mode */}
+        <span style={{ color: darkMode ? "white" : "" }}>My Awesome</span>
+        <span>Services</span>
+        <spane style={{paddingBottom:"10px"}}>
+
+         
+
+         Proficient in HTML, CSS, JavaScript, Bootstrap, Tailwind CSS, jQuery, React.js, PHP, Laravel,
+        <br />
+        WordPress, MySQL Database, Git, GitHub, VS Code, and Chrome DevTools,
+          <br />
+          I offer comprehensive expertise in full-stack web development.
+        </spane>
+        <a href={Resume} download>
+          <Button BtnName="Download CV"/>
+          
+        </a>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right */}
+      <div className="cards">
+        {/* first card */}
+        <motion.div
+          initial={{ left: "20rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={HeartEmoji}
+            heading={"Font-End"}
+            detail={"HTML,CSS,Bootstrap,Tailwind CSS, JavaScript,jQuery,React js & WordPress"}
+          />
+        </motion.div>
+        {/* second card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "12rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Glasses}
+            heading={"Back-End & Database"}
+            detail={"PHP,Laravel,WordPress, MySQL Database"}
+          />
+        </motion.div>
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "19rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Humble}
+            heading={"Tools"}
+            detail={
+              "Git,GitHub,VS Code, Chrome Dev tool "
+            }
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
